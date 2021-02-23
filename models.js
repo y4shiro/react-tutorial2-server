f4eeb5504922eec215421c28ca1bb9398ac8d17b
y4shiro <y4shironao@gmail.com>
2021-02-23 20:45:45 +0900

    実行環境に応じて Sequelize の接続方法を変更

--- models.js
+++ models.js
@@ -5,14 +5,18 @@ const { DataTypes } = Sequelize;
 const url =
   process.env.DATABASE_URL ||
   "postgres://postgres:postgres@localhost:5432/review_app";
-export const sequelize = new Sequelize(url, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+
+export const sequelize =
+  process.env.NODE_ENV === 'production' ?
+  new Sequelize(url, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-  },
-});
+  }) :
+  new Sequelize(url);
 
 export const User = sequelize.define(
   "user",
